54eda1274ade89c5eb808e920f7412d387464dc4
Abhishek Shastri <shastri48@gmail.com>
2018-10-08 16:11:13 +0530

    add javascript projects

---
+++ practice/Day1/main.js
@@ -0,0 +1,42 @@
+// var fName = "sachin";
+// var lName = "tiwari";
+// function FullName(a, b) {
+//   return (a + " " + b);
+// }
+// var myName = FullName (fName , lName);
+
+
+
+// var num1 = 10;
+// var num1 = 20;
+// function sum(){
+//   console.log(num1+num2);
+// }
+
+// function sum(a, b){
+//   return a+b;
+// }
+// var TotalSum = sum(num1 , num1) ;
+
+
+
+// var fName = prompt("Enter your first name");
+// var lName = prompt("Enter your last name");
+// var FullName = fName + lName;
+
+
+// var fName = prompt ("Enter your name here");
+// function YourName(a){
+//   return a;
+// }
+// var print = YourName(fName);
+
+
+
+
+var fName = prompt("Enter your name");
+prompt("enter your name");
+alert("hahhaa");
+function print(a){
+  alert(fName);
+}
---
+++ project/Day1/project3/practice.js
@@ -0,0 +1,69 @@
+console.log("I'm Learning Javascript");
+
+// var name = "Mark";
+
+// var name = "John";
+// var admin = name;
+// alert(admin);
+
+
+
+// alert("Hello World");
+
+// var name = prompt("Write your name");
+// function greeting() {
+//   alert("welcome " + name);
+// }
+// greeting();
+
+
+// variable name is not defined correctly
+
+
+// var name = prompt("Write your name");
+//   if(name=="bob"||name=="alice"){
+//   function greeting() {
+//     alert("welcome " + name);
+//   }
+//   greeting();
+// }
+
+
+// first and fourth are correct 
+
+
+// var name = prompt("enter your name");
+// console.log("Hello "+name+", How are you doing?");
+
+
+
+// var name = prompt("Enter a sentence");
+// var count = name.length;
+// console.log(count);
+
+
+// var age = prompt("enter your age in years");
+// var dog = age/7;
+// console.log(dog);
+
+
+// var studentScore = 41;
+// var maxScore = 100;
+// var percent = studentScore/maxScore*100;
+// console.log(percent);
+
+
+// var city = "Dharamshala";
+// var country = "India";
+// alert(city + ", " + country);
+
+
+
+var num1 = 20;
+var num2 = 20;
+function sum(){
+  return num1+num2;
+}
+alert(
+  num1+num2
+)
