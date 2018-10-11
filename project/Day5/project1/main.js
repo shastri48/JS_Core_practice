23f01585c1fdccd2cb216b209fa9a37935f0b4d8
Abhishek Shastri <shastri48@gmail.com>
2018-10-11 15:04:09 +0530

    add javascript project

---
+++ project/Day5/project1/main.js
@@ -0,0 +1,6 @@
+var color = ["red", "blue", "blue", "green", "yellow"];
+console.log(color[0]);
+console.log(color[2]);
+color[color.length-1] = "ultraviolet";
+var fourthcolor = color[3];
+color.push("skyblue");
