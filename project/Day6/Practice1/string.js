d70fbc1841271e93a393fea334be02d5af151dc9
Abhishek Shastri <shastri48@gmail.com>
2018-10-17 17:43:32 +0530

    add Day6 project

--- project/Day6/Practice1/string.js
+++ project/Day6/Practice1/string.js
@@ -1,3 +1,4 @@
+
 // Write a JavaScript function to check whether an `input` is a string or not.
   var sentance = prompt("Enter something");
   function stringCheck(sentance) {
@@ -39,6 +40,13 @@ function stringExtract(str) {
 // Write a JavaScript function to convert a Full Name like (Rahul Dravid) -> (Rahul D.)
 // Input -> (String)
 // Output (String)
+function nameString() {
+  str = str.split(" ");
+  for(i = 1; i < str.length; i++){
+    str[i] = str[i].charAt(0).concat(".");
+  }
+  str = str.join(" ");
+}
 
 
 // Write a JavaScript function to hide email addresses to protect from unauthorized user.
@@ -85,7 +93,7 @@ function capitalize() {
 function caseChange() {
   array = str.split(""); 
   for(var i = 0; i < array.length; i++) {
-    if (array[i].toUpperCase()) {
+    if (array[i] == array[i].toLowerCase()) {
       array[i] = array[i].toUpperCase();
     } else {
       array[i] = array[i].toLowerCase();
@@ -98,21 +106,146 @@ function caseChange() {
 // Write a JavaScript function to convert a string into camel case.
 // Input (String) -> 'Learning about js'
 // Output -> 'LearningAboutJs'
+function capitalize() {
+  array = str.split(" ");
+  for(var i = 0; i< array.length; i++){
+    array[i] = array[i].charAt(0).toUpperCase().concat(array[i].substring(1));
+  }
+  str = array.join("");
+  return str;
+}
 
 
 
 // Write a JavaScript function to uncamelize a string
 // Input (String) -> 'LearningAboutJs'
 // Output -> 'Learning About Js'
+// var newstr =[];
+function caseChange() {
+  array = str.split(""); 
+  newstr = [array[0]];
+  for(var i = 1; i < array.length; i++) {
+    if (array[i] == array[i].toUpperCase()) {
+    newstr.push(" ");
+    } 
+    newstr.push(array[i]);
+  }
+  newstr = newstr.join("");
+}
+
 
 
 
 // Write a JavaScript function to concatenates a given string n times
 // Input (String, number) -> ('Hello!', 4)
 // Output -> 'Hello!Hello!Hello!Hello!'
+function repeatMsg(str, number) {
+  str = str.repeat(number);
+  return str;
+}
 
 
 // Write a JavaScript function to humanized number
 // Input -> (Number) -> 1 or 2
 // Output -> (String) -> 1st or 2nd
+function humanizedNum(number) {
+  number = String(number);
+  number = number.split("");
+  if (number[number.length - 1] == "1"){
+    number = number.join("");
+    number = Number(number);
+    return `${number}st`;
+  } else if (number[number.length - 1] == "2"){
+    number = number.join("");
+    number = Number(number);
+    return `${number}nd`;
+  } else if (number[number.length - 1] == "3"){
+    number = number.join("");
+    number = Number(number);
+    return `${number}rd`;
+  } else {
+    number = number.join("");
+    number = Number(number);
+    return `${number}th`;
+  } 
+}
 
+
+
+
+
+
+// Write a JavaScript function to chop a string into chunks of a given length.
+// Input ( String, Number) -> ('Hello World!', 2)
+// Output ['He', 'll', 'o ', 'Wo', 'rl', 'd!']
+  var newStr = [];
+  var newStr2 = [];
+  function chopString(n) {
+    array = str.split("");
+    for(var i = 0; i < array.length; i+=n) {
+      for(var k = i; k < i+n; k++){
+      newStr.push(array[k]);
+    }
+    newStr2.push(newStr.join(""));
+    newStr = [];
+    }
+  }
+
+
+//Write a JavaScript function to count the occurrence of a substring in a string.
+// Input (String, String) -> ('The world of the dogs', 'the')
+// Output (Number) -> 2
+var newStr = [];
+function countRepeat(str1, str2) {
+  str1 = str1.toLowerCase();
+  str1 = str1.split(" ");
+  newStr = str1.filter(word => word == str2.toLowerCase());
+  return newStr.length;
+}
+
+
+
+//  Write a JavaScript function to strip leading and trailing spaces from a string.
+// Input (String) -> ('Hello World   ')
+// Output String -> "Hello World"
+function trimString(string) {
+  newString = string.trim;
+}
+
+
+// Write a JavaScript function to truncate a string to a certain number of words.
+// Input (String, Number) -> ('The quick brown fox jumps over the lazy dog', 4)
+// Output ( String ) -> "The quick brown fox"
+var newArray = [];
+function truncateString(string, num) {
+  var array = string.split(" ");
+  for(i = 0; i < num; i++)
+  newArray.push(array[i]);
+}
+
+
+
+// Write a JavaScript function to alphabetize a given string.(A -z)
+// Input (String) -> 'United States'
+// Output 'SUadeeinsttt'
+function alphabetize() {
+  string = string.split(" ");
+  string = string.join("");
+  array = string.split("");
+  var sorted = array.sort();
+  return sorted.join("");
+}
+
+
+
+// Write a JavaScript function to test case insensitive (except special Unicode characters) string comparison.
+// Input ( String String) -> ('abcd', 'AbcD')
+// Output Boolean -> true
+// ('ABCD', 'Abce') -> false
+
+function insensitive() {
+  str1 = str1.toUpperCase();
+  str2 = str2.toUpperCase();
+  if(str1 == str2) return true;
+  else return false;
+}
