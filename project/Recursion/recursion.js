d87227509e8485b3635f140903d380bdbb1c3c4a
Abhishek Shastri <shastri48@gmail.com>
2018-10-24 23:53:27 +0530

    add bookly project

---
+++ project/Recursion/recursion.js
@@ -0,0 +1,54 @@
+//1. Write a function that loops through the numbers n down to 0.
+// function print(i) {
+//   console.log(i);
+//   if(i <= 0) return;
+//   print(--i);
+// }
+
+
+
+//2. Write a function countTo that takes one argument limit and prints number from 0 to limit
+// var zero = 0;
+// function print(i, limit) {
+//   console.log(i);
+//   if(i > limit) return;
+//   print(++i, limit);
+// }
+
+
+//3. Write a function 'exponent' that takes two arguments base, and expo,
+//uses a while loop to return the exponenet value of the base.
+
+
+
+
+//4. Write a function factorial that take 1 argument 'number' and gives the factorial of the number.
+// var sum = 0;
+// function factorial(num1) {
+//   if(num1 >= 0) return;
+//   sum *= num1;
+//   console.log(sum);
+//   factorial(--num1);
+// }
+
+
+// function factorial(num1) {
+//   if(num1 < 0) return;
+//   if(num1 === 0) return 1;
+//   return num1 * factorial(--num1);
+// }
+
+
+
+//5. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+
+
+
+//6. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num'
+// and multiplies each arr value into by num and returns an array of the values.
+
+
+
+// 7. Write a function 'recursiveReverse' that takes an array and uses recursion
+// to return its contents in reverse
+
