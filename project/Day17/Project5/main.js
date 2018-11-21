036e7744d924d9c9e5be0aaec7b280559c37238b
Abhishek Shastri <shastri48@gmail.com>
2018-11-21 09:08:09 +0530

    add cat and booklist problem in day17

--- project/Day17/Project5/main.js
+++ project/Day17/Project5/main.js
@@ -4,29 +4,56 @@
 // Write methods that increase and decrease those properties.
 // Write a method that prints out the cat's status in each area. (Be creative e.g. Paws is really hungry, Paws is VERY happy.)
 // Make the functions take arguments that increase or decrease arbitrary amounts
-function Cat (name, tiredness, hunger, lonliness) {
-  this.name = name;
-  this.tiredness = function() {
-    this.tiredness = tiredness + 1;  
-    return this.tiredness;
-  }
-  this.hunger = function() {
-    this.hunger = hunger + 1;
-    return this.hunger;
-  }
-  this.lonliness = function() {
-    this.lonliness = lonliness + 1;
-    return this.lonliness;
-  }
-  this.result = function () {
-    console.log(`${this.name} is really hungry`)
-  }
-}
-
 
 class Cat {
-  constructor(){
-    
+  constructor(name) {
+    this.name = name;
+    this.happiness = 5;
+    this.tiredness = 5;
+    this.lonliness = 5;
+    this.hunger = 5;
+  }
+
+  statusFunc(input) {
+    if (input ==  0) {
+      return "not"
+    } if (input > 0 && input <= 3){
+      return "less"
+    } if (input > 3 && input <= 6) {
+      return "moderate"
+    } if (input > 6 && input <= 9) {
+      return "very"
+    } if (input == 10) {
+      return "extreme"
+    }
+  }
+
+  printStatus() {
+    console.log(`${this.name} is ${this.statusFunc(this.hunger)} hungry.`);
+    console.log(`${this.name} is ${this.statusFunc(this.lonliness)} lonely.`);
+    console.log(`${this.name} is ${this.statusFunc(this.tiredness)} tired.`);
+    console.log(`${this.name} is ${this.statusFunc(this.happiness)} happy.`);
+}
+
+  changeStatus(state, amount) {
+    var x = +(prompt(`Press 1 for increment and press 0 for decrement.`));
+    switch(state) {
+      case "hunger": this.hunger =( x ? (this.hunger + amount) : (this.hunger - amount));
+      break;
+      case "happiness": this.happiness =( x ? (this.happiness + amount) : (this.happiness - amount));
+      break;
+      case "tiredness": this.tiredness =( x ? (this.tiredness + amount) : (this.tiredness - amount));
+      break;
+      case "lonliness": this.lonliness =( x ? (this.lonliness + amount) : (this.lonliness - amount));
+      break;
+    }
+    for(const key in this) {
+      if(!(key == "name")){
+        if(this[key] < 0) this[key] = 0;
+        if(this[key] > 10) this[key] =10;
+      }
+    }
+
   }
 }
 
@@ -64,31 +91,72 @@ class Cat {
   // Change the current book to be the next book to be read
   // Change the next book to be read property to be the first unread book you find in the list of books
 
-  class BookList {
-    constructor(numRead, notRead){
-      this.read = numRead;
-      this.notRead = notRead;
-    }
-    nextBook() {
-      console.log("next book");
-    }
-    currentBook() {
-      console.log("current book");
-    }
-    lastBook() {
-      console.log("last book");
-    }
-    books = [1,2,3,4,5,6,7,8,9];
-    add(book){
-      books.push(book);
-    }
+
+class BookList {
+  constructor() {
+    this.bookArray = [];
+    this.markedRead = 0;
+    this.markedNotRead = 0;
+    this.nextReadBookIndex = 1;
+    this.currentReadBookIndex = 0;
+    this.lastReadBookIndex = 0;
   }
 
-  class Book {
-    constructor(title, genre, author, read){
-      this.title = title;
-      this.genre = genre;
-      this.author = author;
-      this.read = read;
-    }
+  addBook(book) {
+    this.bookArray.push(book);
+    this.updateBookStatus();
   }
+
+  updateBookStatus(){
+    this.nextReadBookIndex = this.findNextBookIndex();
+    this.bookArray.forEach((value, i) => {
+      if(value.readStatus) this.markedRead++;
+    });
+    this.markedNotRead = this.bookArray.length - this.markedRead;
+  }
+
+  findNextBookIndex() {
+    let index;
+    for(let i = 0; i < this.bookArray.length; i++) {
+      if(this.bookArray[i].readStatus == false && i != this.currentReadBookIndex) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+
+  finishCurrentBook(){
+    if(! this.nextReadBookIndex) {
+      console.log("BookList is Empty Add more Books to Read");
+      return;
+    }
+    this.bookArray[this.currentReadBookIndex].readStatus = true;
+    this.bookArray[this.currentReadBookIndex].readDate = new Date(Date.now());
+    this.lastReadBookIndex = this.currentReadBookIndex;
+    this.currentReadBookIndex = this.nextReadBookIndex;
+    this.nextReadBookIndex = this.findNextBookIndex();
+    this.updateBookStatus();
+  }
+}
+
+
+class Book {
+  constructor(title, author, genre, readStatus, readDate) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.readStatus = false;
+    this.readDate
+  }
+}
+
+let library = new BookList();
+let b1 = new Book("s", "sw", "swd");
+library.addBook(b1);
+let b2 = new Book("se", "sew", "sewd");
+library.addBook(b2);
+let b3 = new Book("sr", "srw", "rswd");
+library.addBook(b3);
+let b4 = new Book("sh", "shw", "hswd");
+library.addBook(b4);
